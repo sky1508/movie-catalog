e7d75a097dc5b4d4ee2c4fe2d16615252e7d6a10
sky1508 <sky12345085@yahoo.com>
2019-03-31 19:47:44 +0530

    Adding Movie & Rating model in movie-catalog-service

--- movie-catalog-service/src/main/java/com/sky/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/sky/moviecatalogservice/resource/MovieCatalogResource.java
@@ -1,12 +1,17 @@
 package com.sky.moviecatalogservice.resource;
 
 import com.sky.moviecatalogservice.model.CatalogItem;
+import com.sky.moviecatalogservice.model.Movie;
+import com.sky.moviecatalogservice.model.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -15,8 +20,22 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Transformers","Test",4)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123",4),
+                new Rating("456",5)
         );
+
+        return ratings.stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(),"desc",rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+        /*return Collections.singletonList(
+                new CatalogItem("Transformers","Test",4)
+        );*/
     }
 }
