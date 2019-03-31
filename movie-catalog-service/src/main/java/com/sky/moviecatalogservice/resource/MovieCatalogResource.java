e04559a9395971fb4e44902727ca6d77a017efb8
sky1508 <sky12345085@yahoo.com>
2019-03-31 19:51:37 +0530

    Changing movie-catalog to fetch data from movie-info service

--- movie-catalog-service/src/main/java/com/sky/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/sky/moviecatalogservice/resource/MovieCatalogResource.java
@@ -3,13 +3,13 @@ package com.sky.moviecatalogservice.resource;
 import com.sky.moviecatalogservice.model.CatalogItem;
 import com.sky.moviecatalogservice.model.Movie;
 import com.sky.moviecatalogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,10 +18,11 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
-        RestTemplate restTemplate = new RestTemplate();
 
         List<Rating> ratings = Arrays.asList(
                 new Rating("123",4),
